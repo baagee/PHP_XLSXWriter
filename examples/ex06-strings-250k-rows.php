409f8db06feb5ad42b5b8fd4977c50fbbc01cd91
党留辉 <liuhuidang@sf-express.com>
2020-07-04 14:36:22 +0800

    update

--- examples/ex06-strings-250k-rows.php
+++ examples/ex06-strings-250k-rows.php
@@ -1,13 +1,13 @@
 <?php
 include __DIR__ . '/../vendor/autoload.php';
-$fileName = __DIR__ . '/excel/' . basename(__FILE__ , '.php') . '.xlsx';
+$fileName = __DIR__ . '/excel/' . basename(__FILE__, '.php') . '.xlsx';
 
 $chars = "abcdefghijklmnopqrstuvwxyz0123456789 ";
 $s = '';
 for ($j = 0; $j < 16192; $j++)
     $s .= $chars[rand() % 36];
 
-
+$t1 = microtime(true);
 $writer = new \BaAGee\Excel\XLSXWriter();
 $writer->writeSheetHeader('Sheet1', array('c1' => 'string', 'c2' => 'string', 'c3' => 'string', 'c4' => 'string'));//optional
 for ($i = 0; $i < 250000; $i++) {
@@ -18,5 +18,7 @@ for ($i = 0; $i < 250000; $i++) {
     $writer->writeSheetRow('Sheet1', array($s1, $s2, $s3, $s4));
 }
 $writer->writeToFile($fileName);
-echo '#' . floor((memory_get_peak_usage()) / 1024 / 1024) . "MB" . "\n";
+$t2 = microtime(true);
+echo '#' . floor((memory_get_peak_usage()) / 1024 / 1024) . "MB" . PHP_EOL;
+echo '#' . (($t2 - $t1)) . "s" . PHP_EOL;
 
